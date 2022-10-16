6a179c6e2790102ba29e9850d6408fecfa92fbbe
meron <merronmuche@gmail.com>
2022-10-16 01:03:57 -0700

    scroll to the side

--- bb.js
+++ bb.js
@@ -1,45 +1,14 @@
-// const panels = document.querySelectorAll('panel')
+const panels = document.querySelectorAll('panel')
 
-// panels.forEach(panel => {
-//     panel.addEventListener('click', () => {
-//         removeActiveClasses()
-//       panel.classList.add('active')
-//     })
-// })
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+      panel.classList.add('active')
+    })
+})
 
-// function removeActiveClasses() {
-//     panels.forEach(panel => {
-//         panel.classList.remove('active')
-//     })
-// }
-class Car {
-    constructor(brand) {
-      this.carname = brand;
-    }
-    present() {
-      return 'I have a ' + this.carname;
-    }
-
-  }
-
-  let a = new Car('djfaklj')
-//   a.carname = 'another'
-//   console.log(a.carname) 
-  console.log(a.present())
-  
-
-  
-//   class Model extends Car {
-//     constructor(brand, mod) {
-//         super(brand);
-//         this.model = mod;
-//       }
-      
-//       show() {
-//         return this.present() + ', it is a ' + this.model;
-//       }
-//   }
-
-// let x = new Model("BMW", 'bm15')
-
-// console.log(x.present())
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}

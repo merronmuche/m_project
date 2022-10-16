3ba4d6135544ad310b17df786499a29ac041a83b
meron <merronmuche@gmail.com>
2022-10-16 02:30:21 -0700

    Done using Dom for two photos

--- bb.js
+++ bb.js
@@ -1,10 +1,27 @@
-const panels = document.querySelectorAll('.panel')
+let panels = document.querySelectorAll('.panel')
+const cont = document.getElementById('cont')
 
-panels.forEach(panel => {
+let urls = ["url('http://127.0.0.1:5500/images/mark.jpg')",
+ "url('http://127.0.0.1:5500/images/abi.jpg')",
+]
+names = ['Markos', 'Abi']
+let idx = 0;
+
+urls.forEach(url => {
+    let panel = document.createElement('div')
+    panel.classList.add('panel')
+    if (idx == 0){
+        panel.classList.add('active')
+    }
+    cont.append(panel)
     panel.addEventListener('click', () => {
         removeActiveClasses()
       panel.classList.add('active')
     })
+    panel.style.backgroundImage = urls[idx];
+    panel.innerHTML=names[idx]
+    cont.appendChild(panel)
+    idx++;
 })
 
 function removeActiveClasses() {
